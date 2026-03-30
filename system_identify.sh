601e98f5a3161c67d36a167ecb99c1a2539e19de
Shubham Singh <shubhamsingh4377@gmail.com>
2026-03-30 22:52:10 +0530

    Update system_identify.sh

--- system_identify.sh
+++ system_identify.sh
@@ -24,3 +24,29 @@ printf "%-13s : %s\n" "Uptime" "$system_uptime"
 printf "%-13s : %s\n" "Date/Time" "$current_datetime"
 printf "%-13s : %s\n" "License" "GNU GPL v3 (Linux OS)"
 print_line
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: [Your Name]
+# Course: Open Source Software
+# Purpose: Show basic system identity details
+
+# Store some useful system information in variables
+DISTRO=$(lsb_release -d | cut -f2)   # Linux distribution name
+KERNEL=$(uname -r)                   # Kernel version
+USER_NAME=$(whoami)                  # Current logged-in user
+HOME_DIR=$HOME                       # Home directory of the user
+UPTIME=$(uptime -p)                  # How long the system has been running
+DATE=$(date)                         # Current date and time
+
+# Display the collected information in a neat format
+echo "---------------------------------------------"
+echo "   Open Source Audit - System Identity"
+echo "---------------------------------------------"
+echo "Distribution : $DISTRO"
+echo "Kernel       : $KERNEL"
+echo "User         : $USER_NAME"
+echo "Home Dir     : $HOME_DIR"
+echo "Uptime       : $UPTIME"
+echo "Date/Time    : $DATE"
+echo "License      : GNU GPL v3 (Linux OS)"
+echo "---------------------------------------------"
